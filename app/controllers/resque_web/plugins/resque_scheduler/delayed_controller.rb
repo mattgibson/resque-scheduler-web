cdb0cfe6b6a9789153401afa805fe4bd042c5ace
Matt Gibson <downrightlies@gmail.com>
2015-04-13 20:34:33 +0100

    Docs

--- app/controllers/resque_web/plugins/resque_scheduler/delayed_controller.rb
+++ app/controllers/resque_web/plugins/resque_scheduler/delayed_controller.rb
@@ -5,6 +5,8 @@ module ResqueWeb
       # application, so here, they can be run immediately, deleted from the
       # queue, or rescheduled.
       class DelayedController < ResqueWeb::ApplicationController
+
+        # GET /schedules
         def index
           @start = params[:start].to_i
           @number_to_show = 20
@@ -12,6 +14,7 @@ module ResqueWeb
           @timestamps = Resque.delayed_queue_peek(@start, @number_to_show)
         end
 
+        # GET /delayed/jobs/:klass
         def jobs_klass
           klass = Resque::Scheduler::Util.constantize(params[:klass])
           @args = JSON.load(URI.decode(params[:args]))
@@ -20,10 +23,12 @@ module ResqueWeb
           @timestamps = []
         end
 
+        # POST /delayed/search
         def search
           @jobs = JobFinder.new(params[:search]).find_jobs
         end
 
+        # POST /delayed/cancel_now
         def cancel_now
           klass = Resque::Scheduler::Util.constantize(params['klass'])
           timestamp = params['timestamp']
@@ -32,11 +37,13 @@ module ResqueWeb
           redirect_to Engine.app.url_helpers.delayed_path
         end
 
+        # POST /delayed/clear
         def clear
           Resque.reset_delayed_queue
           redirect_to Engine.app.url_helpers.delayed_path
         end
 
+        # POST /delayed/queue_now
         def queue_now
           timestamp = params['timestamp'].to_i
           if timestamp > 0
@@ -45,6 +52,7 @@ module ResqueWeb
           redirect_to ResqueWeb::Engine.app.url_helpers.overview_path
         end
 
+        # GET /delayed/:timestamp
         def timestamp
           @timestamp = params[:timestamp].to_i
         end
