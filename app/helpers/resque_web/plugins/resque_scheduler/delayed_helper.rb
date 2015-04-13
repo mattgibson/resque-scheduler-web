8afba353399d7f64b600281a2c98142c90230249
Matt Gibson <downrightlies@gmail.com>
2015-04-13 17:45:54 +0100

    Docs

--- app/helpers/resque_web/plugins/resque_scheduler/delayed_helper.rb
+++ app/helpers/resque_web/plugins/resque_scheduler/delayed_helper.rb
@@ -2,6 +2,7 @@ module ResqueWeb
   module Plugins
     module ResqueScheduler
       module DelayedHelper
+        # Outputs the time in a human readable way: 2015-04-12 12:27:05 +0100
         def format_time(t)
           t.strftime('%Y-%m-%d %H:%M:%S %z')
         end
