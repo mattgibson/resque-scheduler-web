16d8f1d45e791f7a6a64a480837b4509fa531e1d
Matt Gibson <downrightlies@gmail.com>
2015-04-13 17:45:48 +0100

    Docs

--- app/models/resque_web/plugins/resque_scheduler/job_finder/working_job_finder.rb
+++ app/models/resque_web/plugins/resque_scheduler/job_finder/working_job_finder.rb
@@ -7,10 +7,20 @@ module ResqueWeb
 
           attr_accessor :search_term
 
+          # The search term will be used to match against the class name of any
+          # jobs that are currently being processed by any of the workers.
           def initialize(search_term)
             @search_term = search_term
           end
 
+          # Returns an array of hashes.
+          # [
+          #   {
+          #     'class' => 'SomeClass',
+          #     'queue' => 'some_queue',
+          #     'where_at' => 'working'
+          #   }
+          # ]
           def find_jobs
             workers_with_jobs_that_match_search_term.collect do |w|
               w.job['payload'].merge(
